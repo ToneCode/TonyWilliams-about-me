15ad8a064f4fe59330a8efa67dea62da0f75278b
Anthony Williams <pull.rebase>
2016-04-13 13:25:43 -0600

    Added about-me page to github

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>About my learning journey</title>
+	</head>
+	<body>
+		<p>I'm excited about this journey!</p>
+		<p>I'm learning all about programming in this class</p>
+		<a href="http://stemuluscenter.org/deep-dive-coding-bootcamp/">link to class bootcamp</a>
+	</body>
+</html>
