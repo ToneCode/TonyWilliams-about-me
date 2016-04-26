f9475cbbea689af94b1a18d0f25a1a73ad7e02a3
Anthony Williams <pull.rebase>
2016-04-26 09:12:48 -0600

    Added css style sheet

--- index.php
+++ index.php
@@ -3,14 +3,13 @@
 	<head>
 		<meta charset="utf-8" />
 		<!-- CSS styles start here -->
-		<style type="text/css">
+		<link type="text/css" href="css/styles.css" rel="stylesheet" />
 			p {color: blue;}
-		</style>
 		<title>About my learning journey</title>
 	</head>
 	<body>
 		<p>I'm excited about this journey!</p>
 		<p>I'm learning all about programming in this class</p>
-		<a href="http://stemuluscenter.org/deep-dive-coding-bootcamp/">link to class bootcamp</a>
+		<a href="http://stemuluscenter.org/deep-dive-coding-bootcamp/">link to bootcamp</a>
 	</body>
 </html>
