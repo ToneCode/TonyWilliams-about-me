dee5b78701a791c8840ec03f802be875c0907608
Anthony Williams <pull.rebase>
2016-04-26 09:45:28 -0600

    Added Image to project

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
+
 		<meta charset="utf-8" />
 		<!-- CSS styles start here -->
 		<link type="text/css" href="css/styles.css" rel="stylesheet" />
 		<title>About my learning journey</title>
 	</head>
 	<body>
+		<img src="img_forest.jpg" alt="Forest" width="600" height="400">
 		<p>I'm excited about this journey!</p>
 		<p>I'm learning all about programming in this class</p>
 		<a href="http://stemuluscenter.org/deep-dive-coding-bootcamp/">link to bootcamp</a>
