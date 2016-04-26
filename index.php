c1e3c7f0d88ae2bb0ea5d6aed19a95b38824c70d
Anthony Williams <pull.rebase>
2016-04-26 09:27:17 -0600

    Added css style sheet

--- index.php
+++ index.php
@@ -10,5 +10,18 @@
 		<p>I'm excited about this journey!</p>
 		<p>I'm learning all about programming in this class</p>
 		<a href="http://stemuluscenter.org/deep-dive-coding-bootcamp/">link to bootcamp</a>
+		<h2>RGB Color Examples</h2>
+
+		<h2 style="background-color:rgb(0, 0, 0);color:white">
+			Background-color set by using rgb(0,0,0)
+		</h2>
+
+		<h2 style="background-color:rgb(128, 128, 128);color:white">
+			Background-color set by using rgb(128,128,128)
+		</h2>
+
+		<h2 style="background-color:rgb(255, 255, 255)">
+			Background-color set by using rgb(255,255,255)
+		</h2>
 	</body>
 </html>
